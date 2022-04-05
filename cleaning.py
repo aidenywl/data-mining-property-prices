2f9efd8d194ee812a41f964d6c7679351c329706
raivatshah <hiraivat@gmail.com>
2022-04-05 22:57:11 +0530

    add func for converting tenure to ratio var

--- cleaning.py
+++ cleaning.py
@@ -1,6 +1,9 @@
 from typing import List
 import pandas as pd
 import re
+from datetime import datetime
+from dateutil import relativedelta
+
 
 def process_bedroom_sum(expr) -> int:
     """
@@ -26,12 +29,43 @@ def process_bedroom_half(expr) -> int:
             return expr
     else:
         return expr
-    
+
+
 def categorical_to_onehot(df: pd.DataFrame, cols: List[str]) -> pd.DataFrame:
     one_hot = pd.get_dummies(df[cols])
     df = df.drop(columns=cols)
     df = df.join(one_hot)
     return df
+
+
+def tenure_to_ratio_var(expr: str) -> str:
+    """
+    Convert tenure to a ratio variable: 
+        999: 999 years or freehold
+        99: start date of tenure unkown, so leaving it as is
+        X: if start date is available, we calculate number of years left
+    """
+    if pd.isna(expr):
+        return '99'  # assume standard 99
+    # get number of years
+    res = re.search(r'(\d+)[^\d]+[yY]ears', expr)
+    if res is None:
+        return 999
+    years = int(res.group(1))
+    start_date = re.search(r'\d{2}/\d{2}/\d{4}', expr)
+    if start_date is None:
+        return years
+    else:
+        # calculate already used up tenure
+        date_info = start_date.group().split('/')
+        start_year, start_month, start_date = int(
+            date_info[2]), int(date_info[1]), int(date_info[0])
+        start_date = datetime(start_year, start_month, start_date)
+        curr_date = datetime(2022, 3, 31)
+        diff = relativedelta.relativedelta(curr_date, start_date)
+        years_left = years - diff.years
+        return years_left
+
 
 def tenure_to_binary(expr: str) -> str:
     """
@@ -39,11 +73,11 @@ def tenure_to_binary(expr: str) -> str:
         tenure_freehold: 999 years or freehold
         tenure_60: houses that are only 60 years.
         tenure_99: houses that have a 99 years leasehold.
-    
+
     If the tenure is missing, we assume a standard 99 as that is the most frequent housing tenure model in Singapore.
     """
     if pd.isna(expr):
-        return '99' # assume standard 99
+        return '99'  # assume standard 99
     # get number of years
     res = re.search(r'(\d+)[^\d]+[yY]ears', expr)
     if res is None:
@@ -55,3 +89,6 @@ def tenure_to_binary(expr: str) -> str:
         return '99'
     else:
         return '60'
+
+
+print(tenure_to_ratio_var('leasehold/99 years'))
